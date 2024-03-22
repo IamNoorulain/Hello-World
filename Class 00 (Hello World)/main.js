adff8badc2c67ce1060abc0fedd59b7ee931dc22
Noorulain <160331698+IamNoorulain@users.noreply.github.com>
2024-03-23 00:57:08 +0500

    Add files via upload

---
+++ Class 00 (Hello World)/main.js
@@ -0,0 +1,8 @@
+var theMessage = "Hello World";
+console.log(theMessage);
+var theString = "Noorulain";
+console.log(theString);
+var theNum = 1001;
+console.log(theNum);
+var theBoolean = true;
+console.log(theBoolean);
---
+++ Class 00 (Hello World)/main.ts
@@ -0,0 +1,18 @@
+let theMessage : string = "Hello World"
+console.log(theMessage);
+
+
+
+let theString : string = "Noorulain"
+console.log(theString);
+
+
+let theNum : number = 1001
+console.log(theNum);
+
+
+let theBoolean : boolean = true
+console.log(theBoolean);
+
+
+
